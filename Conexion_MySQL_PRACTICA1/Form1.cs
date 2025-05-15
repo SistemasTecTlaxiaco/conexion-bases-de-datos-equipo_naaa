9f2c4e4b93573441f89ae6c5330d1404c6983906
nallely-lopez <lopezgarcializethnallely@gmail.com>
2025-05-14 22:30:26 -0600

    Agregar proyecto

---
+++ Conexion_MySQL_PRACTICA1/Form1.cs
@@ -0,0 +1,15 @@
+namespace Conexion_MySQL_PRACTICA1
+{
+    public partial class Conexion_BaseDatos : Form
+    {
+        public Conexion_BaseDatos()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
