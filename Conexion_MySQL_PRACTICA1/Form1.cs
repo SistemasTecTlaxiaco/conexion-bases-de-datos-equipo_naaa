c6496957541934c3f39eb2ff60e687040bd5aa72
nallely-lopez <lopezgarcializethnallely@gmail.com>
2025-05-14 22:56:52 -0600

    Se agregó el archivo al repositorio, al igual que se intaló el conector mySQL DATA

--- Conexion_MySQL_PRACTICA1/Form1.cs
+++ Conexion_MySQL_PRACTICA1/Form1.cs
@@ -1,3 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.ApplicationServices;
+using MySql.Data.MySqlClient;
+
 namespace Conexion_MySQL_PRACTICA1
 {
     public partial class Conexion_BaseDatos : Form
