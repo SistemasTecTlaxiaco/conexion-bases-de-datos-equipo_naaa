f7f75caafd42e3b6e460125cecbd9399f2562ef5
nallely-lopez <lopezgarcializethnallely@gmail.com>
2025-05-14 22:30:26 -0600

    Agregar proyecto

---
+++ Conexion_MySQL_PRACTICA1/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Conexion_MySQL_PRACTICA1
+{
+    partial class Conexion_BaseDatos
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Conexion_BaseDatos
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(912, 800);
+            Name = "Conexion_BaseDatos";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Conexion_BaseDatos";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
